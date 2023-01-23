0dc591e42bcee3593e422972bb0b1a07f3677257
martin-ngigi <martinwainaina001@gmail.com>
2023-01-23 20:43:13 +0300

    Connect to DB success

---
+++ dbConnector.php
@@ -0,0 +1,23 @@
+<?php
+//connection credentials
+$servername = 'localhost';
+$username ='root';
+$password ='';
+$database = 'africas_time_db';
+$dbport = 3306;
+
+//create connection
+$db = new mysqli($servername, $username, $password, $database, $dbport);
+
+//check connection
+if($db->connect_error){
+    header('Content-Type: text/plain');
+    //log error to file/db $e->getMessage()
+    die("END An error occurred. Please try again");
+}
+else{
+    echo "Connection established successfully.";
+}
+
+//url -> http://localhost/USSD/africas-time-ussd/dbConnector.php
+?>
